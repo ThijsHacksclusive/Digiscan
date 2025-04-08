ceb2f0b3b81f1153133fabef0dcb8072df793b0d
ThijsHacksclusive <thijs@hacksclusive.com>
2025-04-08 15:39:14 +0200

    Update test.sh

--- scanner/test.sh
+++ scanner/test.sh
@@ -47,13 +47,13 @@ case "$OS" in
                 ;;
             2)
                 cmd="cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh"
+                elif [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then 
                     osascript <<EOF
 tell application "Terminal"
     activate
     do script "$cmd" in front window
 end tell
 EOF
-fi
                     #osascript -e "tell application \"Terminal\" to do script \"$cmd\""
                 elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
                     osascript <<EOF
@@ -66,7 +66,7 @@ tell application "iTerm2"
     end tell
 end tell
 EOF
-fi
+                fi
                 ;;
             3)
                 osascript -e "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
