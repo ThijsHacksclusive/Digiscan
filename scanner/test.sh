4abca340e376c84b8f805ac633bf5d1ca10f6597
ThijsHacksclusive <thijs@hacksclusive.com>
2025-04-08 13:10:12 +0200

    Update test.sh

--- scanner/test.sh
+++ scanner/test.sh
@@ -14,10 +14,32 @@ launch_cmd() {
 
     case "$OS" in
         wsl)
-            # Headless: run inline
-            echo "▶ Running inline (no terminal GUI in $OS):"
-            wt -w 0 cmd
-            ;;
+            case $scan_type in 
+                1) 
+                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./testssl_scans.sh $website_url" &
+                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'" &
+                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./portscan.sh $website_url"
+                    ;;
+                2)
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh  && ./maxage.sh && ./OCSP_stapling_check.sh  && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && read -p 'press enter to quit'"
+                    ;;
+                3)
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'"
+                    ;;
+                4)
+                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./portscan.sh $website_url && read -p 'press enter to quit'"
+                    ;;
+                5)
+                    #wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./ffuf_scan.sh $website_url && read -p 'press enter to quit'" &
+                    #wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./webanalyse.sh $website_url && read -p 'press enter to quit'"
+                    ;;
+                6)
+                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans/testssl_eisen && bash ./csv_editor.sh && read -p 'press enter to quit'"
+                    ;;
+                *)
+                    echo "Invalid scan type: $scan_type"
+                    ;;
+            esac
         macos)
             # macOS Terminal
             osascript -e "tell application \"Terminal\" to do script \"$cmd\""
