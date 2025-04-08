060d21b255c4f6b2d5f191f3f3ba62f93bfa2a99
ThijsHacksclusive <thijs@hacksclusive.com>
2025-04-08 13:22:08 +0200

    Update test.sh

--- scanner/test.sh
+++ scanner/test.sh
@@ -16,9 +16,9 @@ launch_cmd() {
         wsl)
             case $scan_type in 
                 1) 
-                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./testssl_scans.sh $website_url" &
-                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'" &
-                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./portscan.sh $website_url"
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url" &
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'" &
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./portscan.sh $website_url"
                     ;;
                 2)
                     wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh  && ./maxage.sh && ./OCSP_stapling_check.sh  && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && read -p 'press enter to quit'"
@@ -27,14 +27,14 @@ launch_cmd() {
                     wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'"
                     ;;
                 4)
-                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./portscan.sh $website_url && read -p 'press enter to quit'"
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./portscan.sh $website_url && read -p 'press enter to quit'"
                     ;;
                 5)
-                    #wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./ffuf_scan.sh $website_url && read -p 'press enter to quit'" &
-                    #wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans && echo $website_url && bash ./webanalyse.sh $website_url && read -p 'press enter to quit'"
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./ffuf_scan.sh $website_url && read -p 'press enter to quit'" &
+                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./webanalyse.sh $website_url && read -p 'press enter to quit'"
                     ;;
                 6)
-                    wt.exe --window last new-tab bash -ic "cd /mnt/c/Users/theer/Documents/Stage/scanner/scans/testssl_eisen && bash ./csv_editor.sh && read -p 'press enter to quit'"
+                    wt.exe --window last new-tab bash -ic "cd scans/testssl_eisen && bash ./csv_editor.sh && read -p 'press enter to quit'"
                     ;;
                 *)
                     echo "Invalid scan type: $scan_type"
@@ -69,17 +69,17 @@ case "$scan_type" in
         launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && echo "Enter name" && read name && echo $name"
         ;;
     3)
-        launch_cmd "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
+        launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
         ;;
     4)
-        launch_cmd "cd scans && echo $website_url && bash ./portscan.sh $website_url "
+        launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./portscan.sh $website_url "
         ;;
     5)
-        launch_cmd "cd scans && echo $website_url && bash ./ffuf_scan.sh $website_url" &
-        launch_cmd "cd scans && echo $website_url && bash ./webanalyse.sh $website_url"
+        launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./ffuf_scan.sh $website_url" &
+        launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./webanalyse.sh $website_url"
         ;;
     6)
-        launch_cmd "cd scans/testssl_eisen && bash ./csv_editor.sh"
+        launch_cmd "cd Digiscan && cd scanner && cd scans/testssl_eisen && bash ./csv_editor.sh"
         ;;
     *)
         echo "❌ Invalid scan type: $scan_type"
