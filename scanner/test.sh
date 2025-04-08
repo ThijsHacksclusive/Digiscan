91fa34a08da5267f2827e114b1b7267b5e5fd63d
ThijsHacksclusive <thijs@hacksclusive.com>
2025-04-08 12:07:03 +0200

    Update test.sh

--- scanner/test.sh
+++ scanner/test.sh
@@ -43,7 +43,7 @@ case "$scan_type" in
         launch_cmd "cd scans && echo $website_url && bash ./portscan.sh $website_url"
         ;;
     2)
-        launch_cmd "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && echo "Enter name" && read name && echo $name"
+        launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && echo "Enter name" && read name && echo $name"
         ;;
     3)
         launch_cmd "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
