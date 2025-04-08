7ba19cc2593cbe4561670afbbf066dafa1ab668d
ThijsHacksclusive <thijs@hacksclusive.com>
2025-04-08 14:47:34 +0200

    Update test.sh

--- scanner/test.sh
+++ scanner/test.sh
@@ -9,74 +9,83 @@ if [ -z "$OS" ]; then
     OS="unknown"
 fi
 
-launch_cmd() {
-    local cmd="$1"
-
-    case "$OS" in
-        wsl)
-            case $scan_type in 
-                1) 
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url" &
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'" &
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./portscan.sh $website_url"
-                    ;;
-                2)
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh  && ./maxage.sh && ./OCSP_stapling_check.sh  && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && read -p 'press enter to quit'"
-                    ;;
-                3)
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'"
-                    ;;
-                4)
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./portscan.sh $website_url && read -p 'press enter to quit'"
-                    ;;
-                5)
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./ffuf_scan.sh $website_url && read -p 'press enter to quit'" &
-                    wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./webanalyse.sh $website_url && read -p 'press enter to quit'"
-                    ;;
-                6)
-                    wt.exe --window last new-tab bash -ic "cd scans/testssl_eisen && bash ./csv_editor.sh && read -p 'press enter to quit'"
-                    ;;
-                *)
-                    echo "Invalid scan type: $scan_type"
-                    ;;
-            esac
-            ;;
-        macos)
-            case "$scan_type" in 
-                1) 
-                    launch_cmd "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url" &
-                    launch_cmd "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url" &
-                    launch_cmd "cd scans && echo $website_url && bash ./portscan.sh $website_url"
-                    ;;
-                2)
-                    osascript -e "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && echo "Enter name" && read name && echo $name"
-                    ;;
-                3)
-                    osascript -e "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
-                    ;;
-                4)
-                    launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./portscan.sh $website_url "
-                    ;;
-                5)
-                    launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./ffuf_scan.sh $website_url" &
-                    launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./webanalyse.sh $website_url"
-                    ;;
-                6)
-                    launch_cmd "cd Digiscan && cd scanner && cd scans/testssl_eisen && bash ./csv_editor.sh"
-                    ;;
-                *)
-                    echo "❌ Invalid scan type: $scan_type"
-                    ;;
-            esac
-            ;;
-        *)
-            # Default fallback (Windows Terminal if available)
-            if command -v wt.exe &> /dev/null; then
-                wt.exe --window last new-tab bash -ic "$cmd"
-            else
-                echo "⚠️ No GUI terminal found. Running inline:"
-                bash -c "$cmd"
-            fi
-            ;;
-    esac
+case "$OS" in
+    wsl)
+        case $scan_type in 
+            1) 
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url" &
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'" &
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./portscan.sh $website_url"
+                ;;
+            2)
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh  && ./maxage.sh && ./OCSP_stapling_check.sh  && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh && read -p 'press enter to quit'"
+                ;;
+            3)
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url && read -p 'press enter to quit'"
+                ;;
+            4)
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./portscan.sh $website_url && read -p 'press enter to quit'"
+                ;;
+            5)
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./ffuf_scan.sh $website_url && read -p 'press enter to quit'" &
+                wt.exe --window last new-tab bash -ic "cd scans && echo $website_url && bash ./webanalyse.sh $website_url && read -p 'press enter to quit'"
+                ;;
+            6)
+                wt.exe --window last new-tab bash -ic "cd scans/testssl_eisen && bash ./csv_editor.sh && read -p 'press enter to quit'"
+                ;;
+            *)
+                echo "Invalid scan type: $scan_type"
+                ;;
+        esac
+        ;;
+    macos)
+        case "$scan_type" in 
+            1) 
+                launch_cmd "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url" &
+                launch_cmd "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url" &
+                launch_cmd "cd scans && echo $website_url && bash ./portscan.sh $website_url"
+                ;;
+            2)
+                if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then
+                    osascript -e "tell application \"Terminal\" to do script \"cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh\" in front window"
+                elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
+                    osascript <<EOF
+                tell application "iTerm2"
+                    tell current window
+                        create tab with default profile
+                        tell current session
+                            write text "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh"
+                        end tell
+                    end tell
+                end tell
+                EOF
+                ;;
+            3)
+                osascript -e "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
+                ;;
+            4)
+                launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./portscan.sh $website_url "
+                ;;
+            5)
+                launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./ffuf_scan.sh $website_url" &
+                launch_cmd "cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./webanalyse.sh $website_url"
+                ;;
+            6)
+                launch_cmd "cd Digiscan && cd scanner && cd scans/testssl_eisen && bash ./csv_editor.sh"
+                ;;
+            *)
+                echo "❌ Invalid scan type: $scan_type"
+                ;;
+        esac
+        ;;
+    *)
+        # Default fallback (Windows Terminal if available)
+        if command -v wt.exe &> /dev/null; then
+            wt.exe --window last new-tab bash -ic "$cmd"
+        else
+            echo "⚠️ No GUI terminal found. Running inline:"
+            bash -c "$cmd"
+        fi
+        ;;
+esac
 }
