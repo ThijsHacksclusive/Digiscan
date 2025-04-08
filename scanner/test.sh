2f23aee687ad8610671e3aff4dc86fce81d1b603
ThijsHacksclusive <thijs@hacksclusive.com>
2025-04-08 16:25:57 +0200

    Update test.sh

--- scanner/test.sh
+++ scanner/test.sh
@@ -48,7 +48,14 @@ case "$OS" in
             2)
                 cmd="cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh"
                 if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then 
-                    osascript -e "tell application \"Terminal\" to do script \"$cmd\" in front window"
+                    osascript <<EOF
+tell application "Terminal"
+    activate
+    tell application "System Events" to keystroke "t" using command down
+    delay 0.2
+    do script "$cmd" in selected tab of the front window
+end tell
+EOF
                 elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
                     osascript <<EOF
 tell application "iTerm2"
@@ -68,7 +75,9 @@ EOF
                     osascript <<EOF
 tell application "Terminal"
     activate
-    do script "$cmd" in front window
+    tell application "System Events" to keystroke "t" using command down
+    delay 0.2
+    do script "$cmd" in selected tab of the front window
 end tell
 EOF
                 elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
@@ -90,7 +99,9 @@ EOF
                     osascript <<EOF
 tell application "Terminal"
     activate
-    do script "$cmd" in front window
+    tell application "System Events" to keystroke "t" using command down
+    delay 0.2
+    do script "$cmd" in selected tab of the front window
 end tell
 EOF
                 elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
