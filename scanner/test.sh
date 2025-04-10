ef0cc5e3199d53a40de32b2432ffcb383276ce79
ThijsHacksclusive <thijs@hacksclusive.com>
2025-04-10 13:56:20 +0200

    Update test.sh

--- scanner/test.sh
+++ scanner/test.sh
@@ -58,7 +58,7 @@ case "$OS" in
             2)
                 cmd="cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh"
                 if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then 
-                    ttab "$cmd"
+                    ttab "cd scans && echo $website_url && bash ./testssl_scans.sh $website_url && ./nog_een_test.sh $website_url && ./CSP_check.sh && ./curvestest.sh && ./ff_groupstest.sh && ./maxage.sh && ./OCSP_stapling_check.sh && ./Referrer_policy.sh && ./RSA_keysize_check.sh && ./Secure_renego.sh && ./X_Content_Type_Options.sh && ./alles_bij_elkaar.sh"
                 elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
                     osascript <<EOF
 tell application "iTerm2"
@@ -75,7 +75,7 @@ EOF
             3)
                 cmd="cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
                 if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then 
-                    ttab "$cmd"
+                    ttab "cd scans && echo $website_url && bash ./HTTP_method_scanner.sh $website_url"
                 elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
                     osascript <<EOF
 tell application "iTerm2"
@@ -92,7 +92,7 @@ EOF
             4)
                 cmd="cd Digiscan && cd scanner && cd scans && echo $website_url && bash ./portscan.sh $website_url"
                 if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then 
-                    ttab "$cmd"
+                    ttab "cd scans && echo $website_url && bash ./portscan.sh $website_url"
                 elif [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then 
                     osascript <<EOF
 tell application "iTerm2"
